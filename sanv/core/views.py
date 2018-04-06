4a386a91c652c1b3d7499606b217d5a8483caf9a
Sam Hoang <samhv.ict@gmail.com>
2018-04-06 23:14:57 +0700

    init django

---
+++ sanv/core/views.py
@@ -0,0 +1,9 @@
+from django.template import Context, loader
+from django.shortcuts import render, render_to_response
+
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
